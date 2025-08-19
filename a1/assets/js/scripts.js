18b505da98c0c7800400a48b3fa038050b9b2234
Joemart <joemart2002@yahoo.com>
2025-08-19 13:10:21 +1000

    Commit 4: Added gallery page with modal viewer.

--- a1/assets/js/scripts.js
+++ a1/assets/js/scripts.js
@@ -21,3 +21,11 @@ function includeHTML() {
 }
 
 document.addEventListener("DOMContentLoaded", includeHTML);
+function openModal(img) {
+  document.getElementById("modal").style.display = "flex";
+  document.getElementById("modal-img").src = img.src;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
